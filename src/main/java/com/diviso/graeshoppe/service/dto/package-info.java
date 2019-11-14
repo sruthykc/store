bd0edb4a14bb7a72e3c01d9fceb63cb931ed55bb
sruthykc <sruthy.k.c@gmail.com>
2019-11-14 11:22:13 +0530

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/com/diviso/graeshoppe/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.diviso.graeshoppe.service.dto;
---
+++ src/main/java/com/diviso/graeshoppe/web/rest/vm/ManagedUserVM.java
@@ -0,0 +1,19 @@
+package com.diviso.graeshoppe.web.rest.vm;
+
+import com.diviso.graeshoppe.service.dto.UserDTO;
+
+/**
+ * View Model extending the UserDTO, which is meant to be used in the user management UI.
+ */
+public class ManagedUserVM extends UserDTO {
+
+    public ManagedUserVM() {
+        // Empty constructor needed for Jackson.
+    }
+
+    @Override
+    public String toString() {
+        return "ManagedUserVM{" +
+            "} " + super.toString();
+    }
+}
