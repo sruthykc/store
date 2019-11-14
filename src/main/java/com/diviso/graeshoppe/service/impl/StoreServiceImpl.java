d6a4b50ac47c9ad52fa76d6ab4538fcb4bf9323e
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-11-14 14:30:56 +0530

    add store-schema.avsc

--- src/main/java/com/diviso/graeshoppe/service/impl/StoreServiceImpl.java
+++ src/main/java/com/diviso/graeshoppe/service/impl/StoreServiceImpl.java
@@ -22,67 +22,99 @@ import java.util.Optional;
 @Transactional
 public class StoreServiceImpl implements StoreService {
 
-    private final Logger log = LoggerFactory.getLogger(StoreServiceImpl.class);
+	private final Logger log = LoggerFactory.getLogger(StoreServiceImpl.class);
 
-    private final StoreRepository storeRepository;
+	private final StoreRepository storeRepository;
 
-    private final StoreMapper storeMapper;
+	private final StoreMapper storeMapper;
 
-    public StoreServiceImpl(StoreRepository storeRepository, StoreMapper storeMapper) {
-        this.storeRepository = storeRepository;
-        this.storeMapper = storeMapper;
-    }
+	public StoreServiceImpl(StoreRepository storeRepository, StoreMapper storeMapper) {
+		this.storeRepository = storeRepository;
+		this.storeMapper = storeMapper;
+	}
 
-    /**
-     * Save a store.
-     *
-     * @param storeDTO the entity to save
-     * @return the persisted entity
-     */
-    @Override
-    public StoreDTO save(StoreDTO storeDTO) {
-        log.debug("Request to save Store : {}", storeDTO);
-        Store store = storeMapper.toEntity(storeDTO);
-        store = storeRepository.save(store);
-        return storeMapper.toDto(store);
-    }
+	/**
+	 * Save a store.
+	 *
+	 * @param storeDTO
+	 *            the entity to save
+	 * @return the persisted entity
+	 */
+	@Override
+	public StoreDTO save(StoreDTO storeDTO) {
+		log.debug("Request to save Store : {}", storeDTO);
+		Store store = storeMapper.toEntity(storeDTO);
+		store = storeRepository.save(store);
+		StoreDTO result = storeMapper.toDto(store);
 
-    /**
-     * Get all the stores.
-     *
-     * @param pageable the pagination information
-     * @return the list of entities
-     */
-    @Override
-    @Transactional(readOnly = true)
-    public Page<StoreDTO> findAll(Pageable pageable) {
-        log.debug("Request to get all Stores");
-        return storeRepository.findAll(pageable)
-            .map(storeMapper::toDto);
-    }
+		String status = "create";
 
+		/*boolean publishstatus = createPublishMesssage(result, status);
 
-    /**
-     * Get one store by id.
-     *
-     * @param id the id of the entity
-     * @return the entity
-     */
-    @Override
-    @Transactional(readOnly = true)
-    public Optional<StoreDTO> findOne(Long id) {
-        log.debug("Request to get Store : {}", id);
-        return storeRepository.findById(id)
-            .map(storeMapper::toDto);
-    }
+		log.debug("------------------------------------------published" + publishstatus);*/
+		return result;
 
-    /**
-     * Delete the store by id.
-     *
-     * @param id the id of the entity
-     */
-    @Override
-    public void delete(Long id) {
-        log.debug("Request to delete Store : {}", id);        storeRepository.deleteById(id);
-    }
+	}
+/*public boolean createPublishMesssage(com.diviso.graeshoppe.domain.Customer customer, String status) {
+		
+        log.debug("------------------------------------------publish method"+status);
+
+		com.diviso.graeshoppe.avro.CustomerInfo message =customerAvroMapper.toAvro(customer);
+		message .setStatus(status);
+
+		System.out.println("avro mapped#############################################"+message);
+
+		return messageChannel.customerOut().send(MessageBuilder.withPayload(message).build());
+		
+
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Get all the stores.
+	 *
+	 * @param pageable
+	 *            the pagination information
+	 * @return the list of entities
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public Page<StoreDTO> findAll(Pageable pageable) {
+		log.debug("Request to get all Stores");
+		return storeRepository.findAll(pageable).map(storeMapper::toDto);
+	}
+
+	/**
+	 * Get one store by id.
+	 *
+	 * @param id
+	 *            the id of the entity
+	 * @return the entity
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<StoreDTO> findOne(Long id) {
+		log.debug("Request to get Store : {}", id);
+		return storeRepository.findById(id).map(storeMapper::toDto);
+	}
+
+	/**
+	 * Delete the store by id.
+	 *
+	 * @param id
+	 *            the id of the entity
+	 */
+	@Override
+	public void delete(Long id) {
+		log.debug("Request to delete Store : {}", id);
+		storeRepository.deleteById(id);
+	}
 }
